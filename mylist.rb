ac733cb033009c4ac2e4121315335fde73aedf33
Luis Muñoz <l.munoz.m@outlook.com>
2023-01-26 17:41:50 -0300

    Create MyList class

---
+++ mylist.rb
@@ -0,0 +1,32 @@
+require './my_enumerable'
+class MyList
+  include MyEnumerable
+  def initialize(*list)
+    @list = list
+  end
+
+  attr_reader :list
+
+  def each
+    yield(@list)
+  end
+end
+
+list = MyList.new(1, 2, 3, 4)
+pp list
+
+# Test #all?
+# true
+pp list.all? { |e| e < 5 }
+# false
+pp list.all? { |e| e > 5 }
+
+# Test #any?
+# true
+pp list.any? { |e| e == 2 }
+# false
+pp list.any? { |e| e == 5 }
+
+# Test #filter
+# => [2, 4]
+list.filter { |e| e.even? }
